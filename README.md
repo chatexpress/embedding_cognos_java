5e56e7ff2ebc029559f14bb53fa3acabee272775
leoesleoesleo <leoesleo1111@gmail.com.com>
2023-10-30 11:11:19 -0500

    version java 8

--- README.md
+++ README.md
@@ -1,1 +1,118 @@
-# embedding_cognos_java
+
+<script src="https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.min.js"></script>
+<script>mermaid.initialize({startOnLoad:true});</script>
+
+#
+# Demo Embedding Cognos Java 8
+
+#### Jersey, Spring Bot
+
+## Flujo del proceso
+<p align="justify">
+Diagrama de contexto
+
+```mermaid
+graph TD
+  A[GET :: AUTENTICAR] --> B[Paso 1]
+  B[CONSTRUIR COOKIES PARA EL REPORTE] --> C[Paso 2]
+  C[GET :: REQUEST AL REPORTE] --> D[Fin]
+```
+
+<p align="justify">
+El alcance es poder embeber reportes de IBM Cognos en una plicación externa	
+</p>
+
+- Estrucutra de las cookies
+	```
+	cookies.put("XSRF-TOKEN", cookiesMap.get("XSRF-TOKEN").getValue());
+    cookies.put("cam_passport", cookiesMap.get("cam_passport").getValue());
+    cookies.put("usersessionid", cookiesMap.get("usersessionid").getValue());
+    cookies.put("CRN", cookiesMap.get("CRN").getValue());
+    cookies.put("cea-ssa", cookiesMap.get("cea-ssa").getValue());
+    cookies.put("userCapabilities", cookiesMap.get("userCapabilities").getValue());
+    cookies.put("userCapabilitiesEx", cookiesMap.get("userCapabilitiesEx").getValue());
+	```
+
+
+## Manual de instalación
+
+- Clonar repositorio
+	```
+	git clone https://github.com/leoesleoesleo/....
+	```
+ 
+- Instalar Java (Ejemplos en Linux Ubuntu)
+	```
+    java -version
+	```
+ 
+    ```
+	sudo apt-get install openjdk-8-jdk
+	```
+
+- Instalar Maven
+	```
+	sudo apt-get install maven
+	```
+ 
+- Instalar Docker (Opcional)
+	```
+	sudo apt-get update
+	```
+ 
+    ```
+	sudo apt-get install docker.io
+	```
+  
+    ```
+	sudo systemctl start docker
+	```
+  
+    ```
+	sudo systemctl enable docker
+	```
+
+### Iniciando proyecto
+
+- Instalar dependencias
+	```
+	mvn clean install
+	```
+
+- correr la app
+	```
+	mvn spring-boot:run
+	```
+
+ - Generar el .jar (Compilar)
+	```
+	mvn clean package
+	```
+
+ - Ubicar el .jar
+	```
+	java - jar .\target\springtest-0.0.1-SNAPSHOT.jar
+	```
+ 
+### Iniciando proyecto con Docker (Opcional)
+
+- Crear imagen
+	```
+	docker build -t springtest:0.0.1 ./
+	```
+
+- Correr aplicación
+	```
+	docker run -p 8080:8080 springtest:0.0.1
+	```
+
+### Recuerda editar urls de los reportes de Cognos en la ruta:
+	```
+	src/main/java/com/example/springtest/flows/HelloWebRestService.java
+	```
+
+### Correr la aplicación
+ - Ejecutar aplicación desde NGROK con el fin de generar una url con certificados válidos (https) para que las imagenes puedan cargar con los certificados de seguridad.
+ - Ejemplo ruta: https://018b-181-128-238-68.ngrok-free.app/backend/api/helloworld
+ - Nota: para que las imagenes carguen correctamente ademas de correr la aplicación en https, tambien será necesario hacer el logueo desde cognos.
+   
--- src/main/java/com/example/springtest/flows/HelloWebRestService.java
+++ src/main/java/com/example/springtest/flows/HelloWebRestService.java
@@ -5,99 +5,54 @@ import javax.ws.rs.core.Response;
 import org.springframework.stereotype.Component;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
 import java.util.Map;
 import java.util.HashMap;
-import org.json.JSONObject; // Importación para JSONObject
 import javax.ws.rs.core.Cookie; // Importación para Cookie
 import javax.ws.rs.core.NewCookie; // Importación para NewCookie
-import java.net.URI;
-import java.util.List; // Importación para List
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.client.Invocation;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import javax.ws.rs.core.UriBuilder;
 
 @Component
 @Path("/helloworld")
 public class HelloWebRestService {
 
-    private static final String USER_COGNOS = "lpatino";
-    private static final String PASSWORD_COGNOS = "lpatino1234";
-
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String helloMessage() {
-    //public ResponseEntity<String> helloMessage() {
+
+        // PASO 1 AUNTENTICAR
+        // URL del servicio
+        String apiUrl = "https://sigtest.mh.gob.sv/ibmcognos/bi/v1/disp/rds/auth/logon";
+
+        // Parámetros en formato XML
+        String xmlData = "<auth:credentials xmlns:auth='http://developer.cognos.com/schemas/ccs/auth/types/1'>" +
+                "   <auth:credentialElements><auth:name>CAMNamespace</auth:name>" +
+                "   <auth:value><auth:actualValue>AD-Test</auth:actualValue></auth:value></auth:credentialElements>" +
+                "   <auth:credentialElements><auth:name>CAMUsername</auth:name>" +
+                "   <auth:value><auth:actualValue>walther.figueroa</auth:actualValue></auth:value></auth:credentialElements>" +
+                "   <auth:credentialElements><auth:name>CAMPassword</auth:name>" +
+                "   <auth:value><auth:actualValue>Administrador@123</auth:actualValue></auth:value></auth:credentialElements>" +
+                "   </auth:credentials>";
+
         // Crear un cliente Jersey
         Client client = ClientBuilder.newClient();
 
-        // PASO 1 :: GENERAR REQUEST PARA OBTENER SESSION KEY
-        String apiUrl = "https://688e-155-190-18-42.ngrok-free.app/api/v1/session";
-
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-
-        String jsonBody = "{"
-            + "\"parameters\": ["
-                + "{\"name\": \"CAMNamespace\", \"value\": \"MHINT\"},"
-                + "{\"name\": \"CAMUsername\", \"value\": \"" + USER_COGNOS + "\"},"
-                + "{\"name\": \"CAMPassword\", \"value\": \"" + PASSWORD_COGNOS + "\"}"
-            + "]"
-        + "}";
-
+        // Construir la solicitud GET con los parámetros
+        Invocation.Builder builder = client.target(apiUrl)
+                .queryParam("xmlData", xmlData)
+                .request(MediaType.APPLICATION_JSON);
+                
         try {
-            // Realizar la petición PUT
-            Response response = client.target(apiUrl)
-                    .request(MediaType.APPLICATION_JSON)
-                    .put(Entity.json(jsonBody));
+            // Realizar la solicitud GET
+            Response response = builder.get();
 
-            // Verificar la respuesta
-            if (response.getStatus() == 201) {
-                String responseBodySessioKey = response.readEntity(String.class);
-                
-                // Analizar el JSON de la respuesta
-                JSONObject jsonResponse = new JSONObject(responseBodySessioKey);
-                // Obtener el valor de la clave "session_key"
-                String sessionKey = jsonResponse.getString("session_key");
-                System.out.println("Valor de session_key: " + sessionKey);
-                                
-
-                // PASO 2 :: GENERAR REQUEST PARA RECUPERAR XSRF-TOKEN
-                String apiUrlReport = "https://688e-155-190-18-42.ngrok-free.app/bi/v1/disp/rds/reportData/report/iFB439BF7B062490B8763D905E839A2CB?fmt=HTML";
-                String authorizationHeader = "IBM-BA-Authorization";
-
-                // Construir la solicitud GET
-                Invocation.Builder builder = client.target(apiUrlReport)
-                        .request(MediaType.APPLICATION_JSON)
-                        .header(authorizationHeader, sessionKey);
-
-                // Realizar la solicitud GET
-                Response csrfTokenResponse = builder.get();
-
-                // Verificar si la respuesta tiene cookies
-                Map<String, NewCookie> responseCookies = csrfTokenResponse.getCookies();
-                
-                // Buscar la cookie específica "XSRF-TOKEN" y guardar su valor en una variable
-                String xsrfToken = null;
-                if (!responseCookies.isEmpty()) {
-                    for (Map.Entry<String, NewCookie> entry : responseCookies.entrySet()) {
-                        String cookieName = entry.getKey();
-                        if (cookieName.equals("XSRF-TOKEN")) {
-                            xsrfToken = entry.getValue().getValue();
-                            break; // Salir del bucle una vez que se encuentre la cookie
-                        }
-                    }
-                }
-                System.out.println("Valor de la cookie XSRF-TOKEN: " + xsrfToken);
+            // Procesar la respuesta
+            if (response.getStatus() == 200) {
+                String responseBody = response.readEntity(String.class);
+                System.out.println("Respuesta LOGON exitosa: " + responseBody);
 
 
-                // PASO 3 :: CONSTRUIR COOKIES PARA EL REPORTE
+                // PASO 2 CONSTRUIR COOKIES PARA EL REPORTE
                 // Crear un mapa de cookies vacío
                 Map<String, String> cookies = new HashMap<>();
 
@@ -105,56 +60,74 @@ public class HelloWebRestService {
                 Map<String, NewCookie> cookiesMap = response.getCookies();
 
                 // Agregar las cookies al mapa de cookies
+                cookies.put("XSRF-TOKEN", cookiesMap.get("XSRF-TOKEN").getValue());
                 cookies.put("cam_passport", cookiesMap.get("cam_passport").getValue());
-                cookies.put("up", cookiesMap.get("up").getValue());
                 cookies.put("usersessionid", cookiesMap.get("usersessionid").getValue());
                 cookies.put("CRN", cookiesMap.get("CRN").getValue());
                 cookies.put("cea-ssa", cookiesMap.get("cea-ssa").getValue());
                 cookies.put("userCapabilities", cookiesMap.get("userCapabilities").getValue());
                 cookies.put("userCapabilitiesEx", cookiesMap.get("userCapabilitiesEx").getValue());
-                cookies.put("XSRF-TOKEN", xsrfToken);
-
+                //cookies.put("up", cookiesMap.get("up").getValue());
+                
                 String cookiesAsString = cookies.toString();
                 System.out.println("Las Cookies con formato son: " + cookiesAsString);
 
+
+                // PASO 3 :: REQUEST AL REPORTE
+                // URL del servicio con los nuevos parámetros
+                String apiUrlReport = "https://sigtest.mh.gob.sv/ibmcognos/bi/v1/disp/rds/reportData/report/i7A96DE90EB364963BA27A68CAB0338B9";
                 
-                // PASO 4 :: REQUEST AL REPORTE
-                // Construir la solicitud GET
+                // Agregar los parámetros en la consulta
+                String version = "3";
+                String format = "HTML";
+                String p_UsuaroCon = "aa017";
+
                 // Construir la solicitud GET con las cookies
-                Invocation.Builder builder_report = client.target(apiUrlReport)
-                        .request(MediaType.APPLICATION_JSON)
-                        .header(authorizationHeader, sessionKey); // Asegúrate de que sessionKey esté definido
+                UriBuilder uriBuilder = UriBuilder.fromUri(apiUrlReport)
+                .queryParam("v", version)
+                .queryParam("fmt", format)
+                .queryParam("p_UsuaroCon", p_UsuaroCon);
+
+                String fullUrl = uriBuilder.toTemplate();
+                System.out.println("URL completa del request REPORT: " + fullUrl);
+
+                // Construir la solicitud GET y agregar la cookie de sesión
+                Invocation.Builder builder2 = client.target(fullUrl).request(MediaType.APPLICATION_JSON);
                 for (Map.Entry<String, String> cookieEntry : cookies.entrySet()) {
-                    builder_report.cookie(new Cookie(cookieEntry.getKey(), cookieEntry.getValue()));
+                    builder2.cookie(new Cookie(cookieEntry.getKey(), cookieEntry.getValue()));
                 }
 
                 // Realizar la solicitud GET
-                Response reportResponse = builder_report.get();
+                Response response2 = builder2.get();
 
-                // obtener la respuesta como HTML
-                String htmlContent = reportResponse.readEntity(String.class);
+                                            
 
-                // Imprimir o procesar el contenido HTML como desees
-                // ...
+                // Procesar la respuesta (puedes manejarla según tus necesidades)
+                if (response2.getStatus() == 200) {
+                    System.out.println("Respuesta exitosa Reporte");
+                    // obtener la respuesta como HTML
+                    String htmlContent = response2.readEntity(String.class);
+                    return htmlContent;
+                } else {
+                    String errorMessageReport = "La solicitud GET REPORT no fue exitosa. Código de estado: " + response.getStatus();
+                    System.out.println(errorMessageReport);
+                    return errorMessageReport;
+                }
 
-                // Cierra el cliente Jersey
-                client.close();
-                
-                // Devuelve una respuesta ResponseEntity con el contenido HTML                
-                /*
-                return ResponseEntity.status(HttpStatus.OK)
-                    .header("Content-Type", "text/html")
-                    .body(htmlContent);
-                */
-                return htmlContent;
-            } else {
-                System.err.println("Error en la solicitud. Código de respuesta: " + response.getStatus());
-                String errorMessage = "Error en la solicitud. Código de respuesta: " + response.getStatus();
+
+
+            } else {                
+                String errorMessage = "La solicitud GET LOGON no fue exitosa. Código de estado: " + response.getStatus();
+                System.out.println(errorMessage);
                 return errorMessage;
             }
-        } finally {
-            // Cerrar el cliente Jersey
-            client.close();
-        }
+
+
+    } finally {
+        // Cerrar el cliente Jersey
+        client.close();
     }
+        
+}
+
 }
