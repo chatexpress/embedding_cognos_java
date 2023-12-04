c1f6b6467231508de3af93647fb13f01f3d52f92
leoesleoesleo <leoesleo1111@gmail.com.com>
2023-12-04 04:42:24 -0500

    embeding dashboard

--- src/main/java/com/example/springtest/flows/HelloWebRestService.java
+++ src/main/java/com/example/springtest/flows/HelloWebRestService.java
@@ -18,115 +18,77 @@ public class HelloWebRestService {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String helloMessage() {
-
-        // PASO 1 AUNTENTICAR
-        // URL del servicio
-        String apiUrl = "https://sigtest.mh.gob.sv/ibmcognos/bi/v1/disp/rds/auth/logon";
-
-        // Parámetros en formato XML
-        String xmlData = "<auth:credentials xmlns:auth='http://developer.cognos.com/schemas/ccs/auth/types/1'>" +
-                "   <auth:credentialElements><auth:name>CAMNamespace</auth:name>" +
-                "   <auth:value><auth:actualValue>AD-Test</auth:actualValue></auth:value></auth:credentialElements>" +
-                "   <auth:credentialElements><auth:name>CAMUsername</auth:name>" +
-                "   <auth:value><auth:actualValue>walther.figueroa</auth:actualValue></auth:value></auth:credentialElements>" +
-                "   <auth:credentialElements><auth:name>CAMPassword</auth:name>" +
-                "   <auth:value><auth:actualValue>Administrador@123</auth:actualValue></auth:value></auth:credentialElements>" +
-                "   </auth:credentials>";
+    public Response helloMessage() {
 
         // Crear un cliente Jersey
         Client client = ClientBuilder.newClient();
+      
+            // PASO 1 :: REQUEST AL DASHBOARD
+            // URL del DASHBOARD
+            String apiUrlReport = "https://sigtest.mh.gob.sv/ibmcognos/bi/?perspective=dashboard&pathRef=.public_folders%2FModulo%2Bde%2BGesti%25C3%25B3n%2BDirecci%25C3%25B3n%2BGeneral%2Bde%2BAduanas%2FModelo%2BVehiculos%2BUsados%2B%2528ARIVU%2529%2FInformes%2BArivus%2FDASHBORAD%2FDASHBOARD%2BARIVU%2BCONTEO&action=view&mode=dashboard&CAMNamespace=AD-Test&CAMUsername=walther.figueroa&CAMPassword=Administrador@123&ui_appbar=false&ui_navbar=false";
+            
+            // Construir la solicitud GET
+            UriBuilder uriBuilder = UriBuilder.fromUri(apiUrlReport);
+            String fullUrl = uriBuilder.toTemplate();
+            System.out.println("URL completa del request REPORT: " + fullUrl);
 
-        // Construir la solicitud GET con los parámetros
-        Invocation.Builder builder = client.target(apiUrl)
-                .queryParam("xmlData", xmlData)
-                .request(MediaType.APPLICATION_JSON);
-                
-        try {
+            Invocation.Builder builder2 = client.target(fullUrl).request(MediaType.APPLICATION_JSON);
+            
             // Realizar la solicitud GET
-            Response response = builder.get();
+            Response response2 = builder2.get();
 
-            // Procesar la respuesta
-            if (response.getStatus() == 200) {
-                String responseBody = response.readEntity(String.class);
-                System.out.println("Respuesta LOGON exitosa: " + responseBody);
+            // Procesar la respuesta (puedes manejarla según tus necesidades)
+            if (response2.getStatus() == 200) {
+                System.out.println("Respuesta exitosa Reporte");                
+                client.close();
 
+                //Devolver respuesta del request
+                //String ResponseContent = response2.readEntity(String.class);
+                //return htmlContent;
 
-                // PASO 2 CONSTRUIR COOKIES PARA EL REPORTE
-                // Crear un mapa de cookies vacío
-                Map<String, String> cookies = new HashMap<>();
+                // Crear la estructura HTML con la etiqueta iframe y el contenido de la respuesta
+                String htmlContent = "<html><head><title>Mi Pagina con Iframe</title>";
 
-                // Obtener las cookies de la respuesta
-                Map<String, NewCookie> cookiesMap = response.getCookies();
-
-                // Agregar las cookies al mapa de cookies
-                cookies.put("XSRF-TOKEN", cookiesMap.get("XSRF-TOKEN").getValue());
-                cookies.put("cam_passport", cookiesMap.get("cam_passport").getValue());
-                cookies.put("usersessionid", cookiesMap.get("usersessionid").getValue());
-                cookies.put("CRN", cookiesMap.get("CRN").getValue());
-                cookies.put("cea-ssa", cookiesMap.get("cea-ssa").getValue());
-                cookies.put("userCapabilities", cookiesMap.get("userCapabilities").getValue());
-                cookies.put("userCapabilitiesEx", cookiesMap.get("userCapabilitiesEx").getValue());
-                //cookies.put("up", cookiesMap.get("up").getValue());
+                htmlContent += "<style>";
+                htmlContent += "body {";
+                htmlContent += "    font-family: Arial, sans-serif;";
+                htmlContent += "    margin: 0;";
+                htmlContent += "    padding: 0;";
+                htmlContent += "    display: flex;";
+                htmlContent += "    flex-direction: column;";
+                htmlContent += "    align-items: center;";
+                htmlContent += "    justify-content: center;";
+                htmlContent += "    height: 100vh;";
+                htmlContent += "    background-color: #f2f2f2;";
+                htmlContent += "}";
+                htmlContent += "h1 {";
+                htmlContent += "    margin-bottom: 20px;";
+                htmlContent += "}";
+                htmlContent += "iframe {";
+                htmlContent += "    width: 80%;";
+                htmlContent += "    height: 60vh;";
+                htmlContent += "    border: none;";
+                htmlContent += "    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);";
+                htmlContent += "}";
+                htmlContent += "</style>";
                 
-                String cookiesAsString = cookies.toString();
-                System.out.println("Las Cookies con formato son: " + cookiesAsString);
+                htmlContent += "</head><body>";
+                htmlContent += "<h1>Mi Pagina con Iframe desde Java</h1>";
 
+                htmlContent += "<iframe src='https://sigtest.mh.gob.sv/ibmcognos/bi/?perspective=dashboard&pathRef=.public_folders%2FModulo%2Bde%2BGesti%25C3%25B3n%2BDirecci%25C3%25B3n%2BGeneral%2Bde%2BAduanas%2FModelo%2BVehiculos%2BUsados%2B%2528ARIVU%2529%2FInformes%2BArivus%2FDASHBORAD%2FDASHBOARD%2BARIVU%2BCONTEO&action=view&mode=dashboard&CAMNamespace=AD-Test&CAMUsername=walther.figueroa&CAMPassword=Administrador@123&ui_appbar=false&ui_navbar=false' width='320%' height='200' frameborder='0' allow='encrypted-media' allowfullscreen='' sandbox='allow-same-origin allow-scripts'></iframe>";
+                htmlContent += "</body></html>";
 
-                // PASO 3 :: REQUEST AL REPORTE
-                // URL del servicio con los nuevos parámetros
-                String apiUrlReport = "https://sigtest.mh.gob.sv/ibmcognos/bi/v1/disp/rds/reportData/report/i7A96DE90EB364963BA27A68CAB0338B9";
-                
-                // Agregar los parámetros en la consulta
-                String version = "3";
-                String format = "HTML";
-                String p_UsuaroCon = "aa017";
+                return Response.ok(htmlContent, MediaType.TEXT_HTML).build();
 
-                // Construir la solicitud GET con las cookies
-                UriBuilder uriBuilder = UriBuilder.fromUri(apiUrlReport)
-                .queryParam("v", version)
-                .queryParam("fmt", format)
-                .queryParam("p_UsuaroCon", p_UsuaroCon);
+            } else {
+                // Crear la estructura HTML del error personalizado
+                String htmlContent = "<html><head><title>Error al cargar el contenido</title></head><body>";
+                htmlContent += "<h1>Error</h1>";
+                htmlContent += "<p>Error.</p>";
+                htmlContent += "</body></html>";
 
-                String fullUrl = uriBuilder.toTemplate();
-                System.out.println("URL completa del request REPORT: " + fullUrl);
-
-                // Construir la solicitud GET y agregar la cookie de sesión
-                Invocation.Builder builder2 = client.target(fullUrl).request(MediaType.APPLICATION_JSON);
-                for (Map.Entry<String, String> cookieEntry : cookies.entrySet()) {
-                    builder2.cookie(new Cookie(cookieEntry.getKey(), cookieEntry.getValue()));
-                }
-
-                // Realizar la solicitud GET
-                Response response2 = builder2.get();
-
-                                            
-
-                // Procesar la respuesta (puedes manejarla según tus necesidades)
-                if (response2.getStatus() == 200) {
-                    System.out.println("Respuesta exitosa Reporte");
-                    // obtener la respuesta como HTML
-                    String htmlContent = response2.readEntity(String.class);
-                    return htmlContent;
-                } else {
-                    String errorMessageReport = "La solicitud GET REPORT no fue exitosa. Código de estado: " + response.getStatus();
-                    System.out.println(errorMessageReport);
-                    return errorMessageReport;
-                }
-
-
-
-            } else {                
-                String errorMessage = "La solicitud GET LOGON no fue exitosa. Código de estado: " + response.getStatus();
-                System.out.println(errorMessage);
-                return errorMessage;
-            }
-
-
-    } finally {
-        // Cerrar el cliente Jersey
-        client.close();
-    }
+                return Response.ok(htmlContent, MediaType.TEXT_HTML).build();
+            }           
         
 }
 
